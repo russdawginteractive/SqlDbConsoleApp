37fe49ecb46424bb1e47542548c03dee076517ea
Russell T. Griffith <russell.griffith@gmail.com>
2017-06-22 13:52:56 -0400

    Adding actual content to Program.cs Including Create Service Principle PowerShell Script.

--- SqlDbConsoleApp/Program.cs
+++ SqlDbConsoleApp/Program.cs
@@ -1,15 +1,136 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Azure.Management.Sql;
+using Microsoft.Azure.Management.Sql.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
 
 namespace SqlDbConsoleApp
 {
     class Program
     {
+        // For details about these four (4) values, see
+        // https://azure.microsoft.com/documentation/articles/resource-group-authenticate-service-principal/
+        static string _subscriptionId = "0189f292-cf76-426f-8531-7ce95fae2a3e";
+        static string _tenantId = "a6d21377-a9ca-482a-b07b-ad435cfc1de4";
+        static string _applicationId = "d6ecb37a-ba59-43b2-be7d-8d817e94812b";
+        static string _applicationSecret = "SummerParty";
+
+        // Create management clients for the Azure resources your app needs to work with.
+        // This app works with Resource Groups, and Azure SQL Database.
+        static ResourceManagementClient _resourceMgmtClient;
+        static SqlManagementClient _sqlMgmtClient;
+
+        // Authentication token
+        static AuthenticationResult _token;
+
+        // Azure resource variables
+        static string _resourceGroupName = "rdi-resource-group";
+        static string _resourceGrouplocation = "North Central US";
+
+        static string _serverlocation = _resourceGrouplocation;
+        static string _serverName = "rtg-sqltest";
+        static string _serverAdmin = "rtg_SysAdmin";
+        static string _serverAdminPassword = "fbWMd0AKc";
+
+        static string _firewallRuleName = "ClientIPAddress_2017-5-3_16-45-10";
+        static string _startIpAddress = "216.8.226.239";
+        static string _endIpAddress = "216.8.226.239";
+
+        static string _databaseName = "RtgSqlConsoleAppDbTest";
+        static string _databaseEdition = DatabaseEdition.Basic;
+        static string _databasePerfLevel = ""; // "S0", "S1", and so on here for other tiers
+
         static void Main(string[] args)
         {
+            // Authenticate:
+            _token = GetToken(_tenantId, _applicationId, _applicationSecret);
+            Console.WriteLine("Token acquired. Expires on:" + _token.ExpiresOn);
+
+            // Instantiate management clients:
+            _resourceMgmtClient = new ResourceManagementClient(new Microsoft.Rest.TokenCredentials(_token.AccessToken));
+            _sqlMgmtClient = new SqlManagementClient(new Microsoft.Rest.TokenCredentials(_token.AccessToken)) { SubscriptionId = _subscriptionId };
+
+            Console.WriteLine("Resource group...");
+            ResourceGroup rg = CreateOrUpdateResourceGroup(_resourceMgmtClient, _subscriptionId, _resourceGroupName, _resourceGrouplocation);
+            Console.WriteLine("Resource group: " + rg.Id);
+
+
+            Console.WriteLine("Server...");
+            Server sgr = CreateOrUpdateServer(_sqlMgmtClient, _resourceGroupName, _serverlocation, _serverName, _serverAdmin, _serverAdminPassword);
+            Console.WriteLine("Server: " + sgr.Id);
+
+            Console.WriteLine("Server firewall...");
+            FirewallRule fwr = CreateOrUpdateFirewallRule(_sqlMgmtClient, _resourceGroupName, _serverName, _firewallRuleName, _startIpAddress, _endIpAddress);
+            Console.WriteLine("Server firewall: " + fwr.Id);
+
+            Console.WriteLine("Database...");
+            Database dbr = CreateOrUpdateDatabase(_sqlMgmtClient, _resourceGroupName, _serverName, _databaseName, _databaseEdition, _databasePerfLevel);
+            Console.WriteLine("Database: " + dbr.Id);
+
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static ResourceGroup CreateOrUpdateResourceGroup(ResourceManagementClient resourceMgmtClient, string subscriptionId, string resourceGroupName, string resourceGroupLocation)
+        {
+            ResourceGroup resourceGroupParameters = new ResourceGroup()
+            {
+                Location = resourceGroupLocation,
+            };
+            resourceMgmtClient.SubscriptionId = subscriptionId;
+            ResourceGroup resourceGroupResult = resourceMgmtClient.ResourceGroups.CreateOrUpdate(resourceGroupName, resourceGroupParameters);
+            return resourceGroupResult;
+        }
+
+        static Server CreateOrUpdateServer(SqlManagementClient sqlMgmtClient, string resourceGroupName, string serverLocation, string serverName, string serverAdmin, string serverAdminPassword)
+        {
+            Server serverParameters = new Server()
+            {
+                Location = serverLocation,
+                AdministratorLogin = serverAdmin,
+                AdministratorLoginPassword = serverAdminPassword,
+                Version = "12.0"
+            };
+            Server serverResult = sqlMgmtClient.Servers.CreateOrUpdate(resourceGroupName, serverName, serverParameters);
+            return serverResult;
+        }
+
+        static FirewallRule CreateOrUpdateFirewallRule(SqlManagementClient sqlMgmtClient, string resourceGroupName, string serverName, string firewallRuleName, string startIpAddress, string endIpAddress)
+        {
+            FirewallRule firewallParameters = new FirewallRule()
+            {
+                StartIpAddress = startIpAddress,
+                EndIpAddress = endIpAddress
+            };
+            FirewallRule firewallResult = sqlMgmtClient.FirewallRules.CreateOrUpdate(resourceGroupName, serverName, firewallRuleName, firewallParameters);
+            return firewallResult;
+        }
+
+        static Database CreateOrUpdateDatabase(SqlManagementClient sqlMgmtClient, string resourceGroupName, string serverName, string databaseName, string databaseEdition, string databasePerfLevel)
+        {
+            // Retrieve the server that will host this database
+            Server currentServer = sqlMgmtClient.Servers.Get(resourceGroupName, serverName);
+
+            // Create a database: configure create or update parameters and properties explicitly
+            Database newDatabaseParameters = new Database()
+            {
+                Location = currentServer.Location,
+                CreateMode = CreateMode.Default,
+                Edition = databaseEdition,
+                RequestedServiceObjectiveName = databasePerfLevel
+
+            };
+            Database dbResponse = sqlMgmtClient.Databases.CreateOrUpdate(resourceGroupName, serverName, databaseName, newDatabaseParameters);
+            return dbResponse;
+        }
+
+        private static AuthenticationResult GetToken(string tenantId, string applicationId, string applicationSecret)
+        {
+            AuthenticationContext authContext = new AuthenticationContext("https://login.windows.net/" + tenantId);
+            _token = authContext.AcquireToken("https://management.core.windows.net/", new ClientCredential(applicationId, applicationSecret));
+            return _token;
         }
     }
 }
